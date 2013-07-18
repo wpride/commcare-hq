f2e0ae5979d72317c5fa1bb4726e0683a0b98657
Cory Zue <czue@dimagi.com>
2013-07-18 14:49:44 -0400

    more indicators

--- mc/reports/sql.py
+++ mc/reports/sql.py
@@ -3,6 +3,50 @@ from corehq.apps.reports.sqlreport import SqlTabularReport, DatabaseColumn
 from corehq.apps.reports.standard import CustomProjectReport, DatespanMixin
 
 
+HF_MONTHLY_SLUGS = [
+    'home_visits_pregnant',
+    'home_visits_postpartem',
+    'home_visits_newborn',
+    'home_visits_children',
+    'home_visits_other',
+    'home_visits_total',
+    "rdt_positive_children",
+    "rdt_positive_adults",
+    "rdt_others",
+    "rdt_total",
+
+    # "diagnosed cases",
+    "diagnosed_malaria_child",
+    "diagnosed_malaria_adult",
+    "diagnosed_diarrhea",
+    "diagnosed_ari",
+    "diagnosed_total",
+
+    # "treated cases",
+    "treated_malaria",
+    "treated_diarrhea",
+    "treated_ari",
+    "treated_total",
+
+    # "transfers",
+    "transfer_malnutrition",
+    "transfer_incomplete_vaccination",
+    "transfer_danger_signs",
+    "transfer_prenatal_consult",
+    "transfer_missing_malaria_meds",
+    "transfer_other",
+    "transfer_total",
+
+    # "deaths",
+    "deaths_newborn",
+    "deaths_children",
+    "deaths_mothers",
+    "deaths_other",
+    "deaths_total",
+    "heath_ed_talks",
+    "heath_ed_participants",
+]
+
 class HeathFacilityMonthly(SqlTabularReport, CustomProjectReport, DatespanMixin):
     exportable = True
     emailable = True
@@ -32,8 +76,7 @@ class HeathFacilityMonthly(SqlTabularReport, CustomProjectReport, DatespanMixin)
         user = DatabaseColumn("User", "user_id", column_type=SimpleColumn)
         columns = [user]
 
-        for slug in ['home_visits_pregnant', 'home_visits_postpartem', 'home_visits_newborn',
-                     'home_visits_children', 'home_visits_other', 'home_visits_total']:
+        for slug in HF_MONTHLY_SLUGS:
             columns.append(DatabaseColumn(slug, '%s_total' % slug))
 
         return columns
