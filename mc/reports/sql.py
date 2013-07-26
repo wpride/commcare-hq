9dfa195a94083b0176d5d0cf3db61b4fbccc4b8a
Cory Zue <czue@dimagi.com>
2013-07-26 13:57:12 -0400

    vertical orientation

--- mc/reports/sql.py
+++ mc/reports/sql.py
@@ -1,34 +1,41 @@
+from dimagi.utils.decorators.memoized import memoized
 from sqlagg.columns import *
+from django.utils.translation import ugettext as _
+from corehq.apps.reports.datatables import DataTablesHeader, DataTablesColumn
 from corehq.apps.reports.sqlreport import SqlTabularReport, DatabaseColumn
 from corehq.apps.reports.standard import CustomProjectReport, DatespanMixin
+from corehq.apps.users.util import user_id_to_username
 
 
 HF_MONTHLY_SLUGS = [
+    # home visits
     'home_visits_pregnant',
     'home_visits_postpartem',
     'home_visits_newborn',
     'home_visits_children',
     'home_visits_other',
     'home_visits_total',
+
+    # rdt
     "rdt_positive_children",
     "rdt_positive_adults",
     "rdt_others",
     "rdt_total",
 
-    # "diagnosed cases",
+    # diagnosed cases
     "diagnosed_malaria_child",
     "diagnosed_malaria_adult",
     "diagnosed_diarrhea",
     "diagnosed_ari",
     "diagnosed_total",
 
-    # "treated cases",
+    # treated cases
     "treated_malaria",
     "treated_diarrhea",
     "treated_ari",
     "treated_total",
 
-    # "transfers",
+    # transfers
     "transfer_malnutrition",
     "transfer_incomplete_vaccination",
     "transfer_danger_signs",
@@ -37,7 +44,7 @@ HF_MONTHLY_SLUGS = [
     "transfer_other",
     "transfer_total",
 
-    # "deaths",
+    # deaths
     "deaths_newborn",
     "deaths_children",
     "deaths_mothers",
@@ -48,6 +55,7 @@ HF_MONTHLY_SLUGS = [
 ]
 
 class HeathFacilityMonthly(SqlTabularReport, CustomProjectReport, DatespanMixin):
+
     exportable = True
     emailable = True
     slug = 'hf_monthly'
@@ -80,3 +88,23 @@ class HeathFacilityMonthly(SqlTabularReport, CustomProjectReport, DatespanMixin)
             columns.append(DatabaseColumn(slug, '%s_total' % slug))
 
         return columns
+
+    @memoized
+    def user_column(self, user_id):
+        return DataTablesColumn(user_id_to_username(user_id))
+
+    @property
+    def headers(self):
+        return DataTablesHeader(DataTablesColumn(_('Indicator')),
+                                *[self.user_column(u['html']) for u in [r[0] for r in self._raw_rows]])
+
+    @property
+    @memoized
+    def _raw_rows(self):
+        return list(super(HeathFacilityMonthly, self).rows)
+
+    @property
+    def rows(self):
+        # transpose rows so the indicators go down and users go across
+        return [[column.data_tables_column.html] + [r[i] for r in self._raw_rows] \
+                for i, column in enumerate(self.columns) if i != 0]
